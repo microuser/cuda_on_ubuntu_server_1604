4cf1ea4a2b1eac48f8bf2619ee23e9583834791c
MicroUser <microuser@users.noreply.github.com>
2018-01-28 18:06:29 -0500

    Update installCuda.sh

--- installCuda.sh
+++ installCuda.sh
@@ -16,9 +16,18 @@
 #echo $MYVAR
 
 stillWhile=$((1)); while [[ "$stillWhile"  == $((1)) ]] && dialog --yesno "while dialog" 9 70   ; do
-  echo doing
-  echo stillWhile:$stillWhile
-  stillWhile=$((0))
+  
+  sudo mkdir /usr/local/mine || exit 1
+  sudo chmod 777 /usr/local/mine || exit 1
+  cd /usr/local/mine || exit 1
+  wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb || exit 1
+  sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb || exit 1
+  sudo apt-get update || exit 1
+  sudo apt-get -y install cuda-8-0 || exit 1
+
+  
+  #stillWhile=$((0))
+  break;
 done;
 echo doneing
 
