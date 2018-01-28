3c58cf670f672f7522c5b924f4d86a05a36cec28
MicroUser <microuser@users.noreply.github.com>
2018-01-28 17:50:40 -0500

    Update installCuda.sh

--- installCuda.sh
+++ installCuda.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
 scriptExitCode=$((0))
-echo scriptExitCode:$scriptExitCode
-autoStart=$(($1+0))
-echo autostart:$autostart
-yesno=$(dialog --yesno 'install cuda' 9 70 --output-fd 1)
-echo yesno $yesno
-echo yesno $yesno
+#echo scriptExitCode:$scriptExitCode
+#autoStart=$(($1+0))
+#echo autostart:$autostart
+#yesno=$(dialog --yesno 'install cuda' 9 70 --output-fd 1)
+#echo yesno $yesno
+#echo yesno $yesno
 
-exityesno=$(dialog --yesno 'install cuda' 9 70 && echo 0 || echo 1)
-echo exityesno $exityesno
-echo exityesno $exityesno
+#exityesno=$(dialog --yesno 'install cuda' 9 70 && echo 0 || echo 1)
+#echo exityesno $exityesno
+#echo exityesno $exityesno
 
-MYVAR=$(dialog --inputbox "THIS OUTPUT GOES TO FD 1" 25 25  --output-fd 1)
-echo $MYVAR
+#MYVAR=$(dialog --inputbox "THIS OUTPUT GOES TO FD 1" 25 25  --output-fd 1)
+#echo $MYVAR
 
-while $(! dialog --yesno "while dialog" 9 70) && false ) ; do
+while $(! dialog --yesno "while dialog" 9 70) && false  ; do
   echo doing
 done;
 echo doneing
 
 
-while $(! dialog --yesno "while dialog" 9 70) && false ) ; do
+while $(! dialog --yesno "while dialog" 9 70) && false  ; do
   echo doing
 done;
 echo doneing
