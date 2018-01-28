dcc871b8d80cdbb5d3ed2859139011fcaba16f2d
MicroUser <microuser@users.noreply.github.com>
2018-01-28 17:46:33 -0500

    Update installCuda.sh

--- installCuda.sh
+++ installCuda.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
 scriptExitCode=$((0))
+echo scriptExitCode:$scriptExitCode
 autoStart=$(($1+0))
-
+echo autostart:$autostart
 yesno=$(dialog --yesno 'install cuda' 9 70 --output-fd 1)
-echo $yesno
-echo $yesno
+echo yesno $yesno
+echo yesno $yesno
 
 exityesno=$(dialog --yesno 'install cuda' 9 70 && echo 0 || echo 1)
-echo $exityesno
-echo $exityesno
+echo exityesno $exityesno
+echo exityesno $exityesno
 
 MYVAR=$(dialog --inputbox "THIS OUTPUT GOES TO FD 1" 25 25  --output-fd 1)
 echo $MYVAR
+
+while dialog --yesno "while dialog" 9 70 ; do
+  echo doing
+done;
+echo doneing
